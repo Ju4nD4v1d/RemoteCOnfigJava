bd2c051ba567e59959a6ff670421247eb091303b
jortegat <jortegat@psl.com.co>
2019-04-23 09:49:24 -0500

    variable renamed.

--- app/src/main/java/com/seon/inspections/MainActivity.java
+++ app/src/main/java/com/seon/inspections/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -21,15 +20,15 @@ public class MainActivity extends AppCompatActivity {
     private static final String WELCOME_MESSAGE_KEY = "welcome_message";
     private static final String WELCOME_MESSAGE_CAPS_KEY = "welcome_message_caps";
 
-    private FirebaseRemoteConfig mFirebaseRemoteConfig;
-    private TextView mWelcomeTextView;
+    private FirebaseRemoteConfig firebaseRemoteConfig;
+    private TextView welcomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWelcomeTextView = findViewById(R.id.welcomeTextView);
+        welcomeTextView = findViewById(R.id.welcomeTextView);
 
         Button fetchButton = findViewById(R.id.fetchButton);
         fetchButton.setOnClickListener(new View.OnClickListener() {
@@ -39,16 +38,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
-        mFirebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        firebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
 
         fetchWelcome();
     }
 
     private void fetchWelcome() {
-        mWelcomeTextView.setText(mFirebaseRemoteConfig.getString(LOADING_PHRASE_CONFIG_KEY));
+        welcomeTextView.setText(firebaseRemoteConfig.getString(LOADING_PHRASE_CONFIG_KEY));
 
-        mFirebaseRemoteConfig.fetchAndActivate()
+        firebaseRemoteConfig.fetchAndActivate()
                 .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
                     @Override
                     public void onComplete(@NonNull Task<Boolean> task) {
@@ -68,12 +67,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateUI() {
-        String welcomeMessage = mFirebaseRemoteConfig.getString(WELCOME_MESSAGE_KEY);
-        if (mFirebaseRemoteConfig.getBoolean(WELCOME_MESSAGE_CAPS_KEY)) {
-            mWelcomeTextView.setAllCaps(true);
+        String welcomeMessage = firebaseRemoteConfig.getString(WELCOME_MESSAGE_KEY);
+        if (firebaseRemoteConfig.getBoolean(WELCOME_MESSAGE_CAPS_KEY)) {
+            welcomeTextView.setAllCaps(true);
         } else {
-            mWelcomeTextView.setAllCaps(false);
+            welcomeTextView.setAllCaps(false);
         }
-        mWelcomeTextView.setText(welcomeMessage);
+        welcomeTextView.setText(welcomeMessage);
     }
 }
